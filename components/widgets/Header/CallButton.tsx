8266fb2b1713bea82c22a945c6432a34e77c5d01
xdianovx <dianov.js@gmail.com>
2025-03-20 13:37:34 +0300

    asd

--- components/widgets/Header/CallButton.tsx
+++ components/widgets/Header/CallButton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const CallButton = () => {
   return (
-    <button className="flex items-center gap-3 leading-none bg-black-text text-white py- rounded-[8px] font-medium pl-4 pr-4">
+    <button className="flex items-center gap-3 leading-none bg-black-text text-white py-4 rounded-[8px] font-medium pl-4 pr-4">
       <div className="w-4 h-4 rounded-full bg-accent"></div>
       <span>Обсудить проект</span>
     </button>
